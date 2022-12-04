9277b58e67b5375710dcd63e3251e8ba2210e83e
ehmicky <ehmicky@gmail.com>
2022-12-04 20:13:22 +0100

    Update type tests

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,13 +1,16 @@
-import { expectType, expectError } from 'tsd'
+import { expectType } from 'tsd'
 
 import setErrorStack from 'set-error-stack'
 
 const error = new Error('test')
 expectType<Error>(setErrorStack(error, ''))
 
-expectError(setErrorStack(error))
-expectError(setErrorStack(error, true))
-expectError(setErrorStack(error, '', ''))
+// @ts-expect-error
+setErrorStack(error)
+// @ts-expect-error
+setErrorStack(error, true)
+// @ts-expect-error
+setErrorStack(error, '', '')
 
 expectType<Error>(setErrorStack(null, ''))
 expectType<true>(setErrorStack(error as Error & { prop: true }, '').prop)
